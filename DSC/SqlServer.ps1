b52f8ab83af1a27fb6125f487468b96ee54f8c6e
Joshua Davis <me@jdav.is>
2021-07-27 05:05:17 -0400

    updating dsc

--- DSC/SqlServer.ps1
+++ DSC/SqlServer.ps1
@@ -15,41 +15,44 @@
     
     Node localhost {
 
-        xSqlDatabase CreateDatabase
+        xSqlServerDatabase CreateDatabase
         {
             Ensure          = 'Present'
             ServerName      = 'sqlsvr1'
             Name            = 'CustomerPortal'
+            InstanceName    = 'MSSSQLSERVER'
 
             PsDscRunAsCredential = $sqlLoginCredential
         }
 
-        xSqlLogin CreateDatabaseLogin
+        xSqlServerLogin CreateDatabaseLogin
         {
             Ensure          = 'Present'
             Name            = 'webapp'
             LoginType       = 'SqlLogin'
             ServerName      = 'sqlsvr1'
+            InstanceName    = 'MSSSQLSERVER'
             LoginCredential = $loginCredential
             LoginMustChangePassword        = $false
             LoginPasswordExpirationEnabled = $false
             LoginPasswordPolicyEnforced    = $true
 
             PsDscRunAsCredential = $sqlLoginCredential
-            DependsOn       = '[SqlDatabase]CreateDatabase'
+            DependsOn       = '[xSqlServerDatabase]CreateDatabase'
         }
 
-        xSqlDatabaseUser CreateDatabaseUser
+        xSqlServerDatabaseUser CreateDatabaseUser
         {
             Ensure          = 'Present'
             ServerName      = 'sqlsvr1'
+            InstanceName    = 'MSSSQLSERVER'
             DatabaseName    = 'CustomerPortal'
             Name            = 'webapp'
             UserType        = 'Login'
             LoginName       = 'webapp'
 
             PsDscRunAsCredential = $sqlLoginCredential
-            DependsOn       = '[SqlLogin]CreateDatabaseLogin'
+            DependsOn       = '[xSqlServerLogin]CreateDatabaseLogin'
         }
       }
 }
